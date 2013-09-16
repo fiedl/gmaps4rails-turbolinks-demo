fd729bc2180f41afea01decfb2a7fc8f1ae49f16
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-09-16 12:49:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmapsTurbolinksDemo::Application.config.secret_token = '1f420302a8ea96fc2d303709e24b70236cbf0394ae91c8196bfa55f2f3cf1cb466d0adf738f5d365e0280f950b6ffe98901aee56dff8e602d6b027560c2d0c63'
